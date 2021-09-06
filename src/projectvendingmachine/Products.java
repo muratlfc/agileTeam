65d7ea5e212497a53ddfbd9941157db768d0f440
murat <arkantarkan.33@gmail.com>
2021-09-06 11:51:40 +0100

    V10

---
+++ src/projectvendingmachine/Products.java
@@ -0,0 +1,139 @@
+package projectvendingmachine;
+
+public class Products {
+
+    private double fanta1 = 2.0;
+    private double water1 = 1.0;
+    private double arizona1 = 1.5;
+    private double snicker1 = 2.5;
+    private double chipsAhoy1 = 2.2;
+    private double pringles1 = 12.99;
+    private double milano1 =14.3;
+    private double reeses1= 3.9;
+    private double cocaCola1 = 2.2;
+    private int productNum;
+    private double price;
+
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "snicker1=" + snicker1 +
+                ", chipsAhoy1=" + chipsAhoy1 +
+                ", pringles1=" + pringles1 +
+                ", milano1=" + milano1 +
+                ", reeses1=" + reeses1 +
+                ", cocaCola1=" + cocaCola1 +
+                ", fanta1=" + fanta1 +
+                ", water1=" + water1 +
+                ", arizona1=" + arizona1 +
+                ", skittles1=" + skittles1 +
+                '}';
+    }
+
+
+
+
+
+    public int getProductNum() {
+        return productNum;
+    }
+
+    public void setProductNum(int productNum) {
+        this.productNum = productNum;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public double getSnicker1() {
+        return snicker1;
+    }
+
+    public void setSnicker1(double snicker1) {
+        this.snicker1 = snicker1;
+    }
+
+    public double getChipsAhoy1() {
+        return chipsAhoy1;
+    }
+
+    public void setChipsAhoy1(double chipsAhoy1) {
+        this.chipsAhoy1 = chipsAhoy1;
+    }
+
+    public double getPringles1() {
+        return pringles1;
+    }
+
+    public void setPringles1(double pringles1) {
+        this.pringles1 = pringles1;
+    }
+
+    public double getMilano1() {
+        return milano1;
+    }
+
+    public void setMilano1(double milano1) {
+        this.milano1 = milano1;
+    }
+
+    public double getReeses1() {
+        return reeses1;
+    }
+
+    public void setReeses1(double reeses1) {
+        this.reeses1 = reeses1;
+    }
+
+    public double getCocaCola1() {
+        return cocaCola1;
+    }
+
+    public void setCocaCola1(double cocaCola1) {
+        this.cocaCola1 = cocaCola1;
+    }
+
+    public double getFanta1() {
+        return fanta1;
+    }
+
+    public void setFanta1(double fanta1) {
+        this.fanta1 = fanta1;
+    }
+
+    public double getWater1() {
+        return water1;
+    }
+
+    public void setWater1(double water1) {
+        this.water1 = water1;
+    }
+
+    public double getArizona1() {
+        return arizona1;
+    }
+
+    public void setArizona1(double arizona1) {
+        this.arizona1 = arizona1;
+    }
+
+    public double getSkittles1() {
+        return skittles1;
+    }
+
+    public void setSkittles1(double skittles1) {
+        this.skittles1 = skittles1;
+    }
+
+    private double skittles1 = 1.99;
+
+
+
+}
---
+++ src/projectvendingmachine/Runner.java
@@ -0,0 +1,11 @@
+package projectvendingmachine;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
